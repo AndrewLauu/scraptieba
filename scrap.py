bda0529d090607c6d10e97c45e46d0e16548dd49
Andrew Lau/Termux <Andrew.PF.Lau@gmail.com>
2021-10-24 22:00:41 +0800

    modi

--- scrap.py
+++ scrap.py
@@ -1,7 +1,37 @@
 import requests
 from lxml import etree
+import re
 
-def main():
+def getPage(formulaUrl:str)->int:
+    res=requests.get(formulaUrl)
+    html=etree.HTML(res.text)
+    href=html.xpath('//a[@class="last pagination-item "]/@href')[0]
+    n=re.search('(?<=pn=)\d+',href).group()
+    return int(n)
+
+def getThreadList(pageUrl:str)->dict:
+    ref='https://tieba.baidu.com'
+    res=requests.get(pageUrl)
+    html=etree.HTML(res.text)
+    title=html.xpath('//a[@class="j_th_tit "]/@title')
+    url=html.xpath('//a[@class="j_th_tit "]/@href')
+    d=dict(zip(title,url))
+    return d
+
+def getPosts(postUrl:str)->dict:
+    pass
 
 if __name__=='__main__':
-    main()
+    url=input('url of forum')
+    threadUrlList={}
+    nPage=getPage()
+    postDict={}
+    for n in range(1,nPage+1):
+        nUrl=url+'?pn='+n
+        threadUrlList|=getThreadUrls()
+
+    for threadUrl in threadUrlList:
+        postDict+=getPosts()
+
+
+
